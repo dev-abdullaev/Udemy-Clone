74c208451112dce7544b2430b79dc29dcb45ec4c
Zokhid Abdullaev <77606851+dev-abdullaev@users.noreply.github.com>
2022-03-14 12:19:51 +0500

    Update Settings.py

--- config/settings.py
+++ config/settings.py
@@ -153,14 +153,14 @@ LOGIN_URL = "login"
 
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-EMAIL_HOST_USER = env('HOST_USER')
-EMAIL_HOST_PASSWORD = env('HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = env('HOST_USER')
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = False
+# EMAIL_HOST_USER = env('HOST_USER')
+# EMAIL_HOST_PASSWORD = env('HOST_PASSWORD')
+# DEFAULT_FROM_EMAIL = env('HOST_USER')
 
 
 
