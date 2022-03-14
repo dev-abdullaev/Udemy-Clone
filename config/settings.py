5d88d4b26f7d7174247c616650c5ddea50a6b0e7
Zokhid Abdullaev <77606851+dev-abdullaev@users.noreply.github.com>
2022-03-14 17:36:07 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -90,12 +90,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {"ENGINE": "django.db.backends.postgresql_psycopg2", 
-    "NAME": env('DB_NAME'),
-    'USER': env('DB_USER'),
-    'PASSWORD': env('DB_PASSWORD'),
-    'HOST': env('DB_HOST'),
-    'PORT': '5432'
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 # Password validation
