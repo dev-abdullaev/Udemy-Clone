9d639e3b9b1d0c581b484457215796bd17f6758c
Zokhid <developer.abdullaev@gmail.com>
2022-03-18 15:43:14 +0500

    Change admin urlpath

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     
-    path('admin/', admin.site.urls),
+    path('mine/', admin.site.urls),
     path('', include('course.urls')),
     path('', include('cart.urls')),
     
